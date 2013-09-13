37aa61b5a0f4a002da9ee52365b1e6e2e1ca9a07
Paul Reioux <reioux@gmail.com>
2013-09-12 20:20:55 -0500

    HACK USB OTG: fix logic derp
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	drivers/usb/otg/msm_otg.c

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -1224,7 +1224,7 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 		return;
 
 	// remove charge limit (500mA) in host mode -ziddey
-	if (otg_hack_active) {
+	if (!otg_hack_active) {
 		if ((motg->chg_type == USB_ACA_DOCK_CHARGER ||
 			motg->chg_type == USB_ACA_A_CHARGER ||
 			motg->chg_type == USB_ACA_B_CHARGER ||
@@ -1240,7 +1240,14 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
-
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge == 1) {
+		mA = USB_FASTCHG_LOAD;
+		pr_info("USB fast charging is ON - 1000mA.\n");
+	} else {
+		pr_info("USB fast charging is OFF.\n");
+	}
+#endif
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 
 	pm8921_charger_vbus_draw(mA);
