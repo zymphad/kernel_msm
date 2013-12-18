97bf31dafd1dc1a47fff7dba85355b6cd6011501
anarkia1976 <stefano.villa1976@gmail.com>
2013-12-18 06:12:45 +0100

    msm: acpuclock-8064: added low cpu clock frequencies

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -46,6 +46,12 @@
 
 #define SECCLKAGD		BIT(4)
 
+#ifdef CONFIG_LOW_CPUCLOCKS
+#define FREQ_TABLE_SIZE		39
+#else
+#define FREQ_TABLE_SIZE		35
+#endif
+
 static DEFINE_MUTEX(driver_lock);
 static DEFINE_SPINLOCK(l2_lock);
 
@@ -981,7 +987,7 @@ ssize_t acpuclk_set_vdd(char *buf)
 #endif
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 
 static void __init cpufreq_table_init(void)
 {
