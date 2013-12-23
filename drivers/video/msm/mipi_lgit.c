c709a2a47b6c4ed4e408dafb9269e2c3ff1c4e6e
Michael <djmizry@gmail.com>
2013-12-22 17:23:19 -0800

    Revert "gamma control: add checksum to validate data integrity"
    This reverts commit 506b2d2e8dc2cade965dd7a1d7d8d6d3ec3e659d.

--- drivers/video/msm/mipi_lgit.c
+++ drivers/video/msm/mipi_lgit.c
@@ -230,19 +230,6 @@ struct syscore_ops panel_syscore_ops = {
 	.shutdown = mipi_lgit_lcd_shutdown,
 };
 
-static bool calc_checksum(int intArr[]) {
-	int i = 0;
-	unsigned char chksum = 0;
-
-	for (i=i;i<10;i++)
-		chksum += intArr[i];
-
-	if (chksum == (unsigned char)intArr[0])
-		return true;
-	else
-		return false;
-}
-
 /******************* begin sysfs interface *******************/
 
 static ssize_t kgamma_r_store(struct device *dev, struct device_attribute *attr,
@@ -256,19 +243,18 @@ static ssize_t kgamma_r_store(struct device *dev, struct device_attribute *attr,
 		&kgamma[4], &kgamma[5], &kgamma[6], &kgamma[7],
 		&kgamma[8], &kgamma[9]);
 
-	if (calc_checksum(kgamma)) {
-		kgamma[0] = 0xd0;
-		for (i=0; i<10; i++) {
-			//pr_info("kgamma_r_p [%d] => %d \n", i, kgamma[i]);
-			faux123_power_on_set_1[5].payload[i] = kgamma[i];
-		}
-
-		kgamma[0] = 0xd1;
-		for (i=0; i<10; i++) {
-			//pr_info("kgamma_r_n [%d] => %d \n", i, kgamma[i]);
-			faux123_power_on_set_1[6].payload[i] = kgamma[i];
-		}
+	kgamma[0] = 0xd0;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_r_p [%d] => %d \n", i, kgamma[i]);
+		faux123_power_on_set_1[5].payload[i] = kgamma[i];
 	}
+
+	kgamma[0] = 0xd1;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_r_n [%d] => %d \n", i, kgamma[i]);
+		faux123_power_on_set_1[6].payload[i] = kgamma[i];
+	}
+
 	return count;
 }
 
@@ -298,19 +284,18 @@ static ssize_t kgamma_g_store(struct device *dev, struct device_attribute *attr,
 		&kgamma[4], &kgamma[5], &kgamma[6], &kgamma[7],
 		&kgamma[8], &kgamma[9]);
 
-	if (calc_checksum(kgamma)) {
-		kgamma[0] = 0xd2;
-		for (i=0; i<10; i++) {
-			//pr_info("kgamma_g_p [%d] => %d \n", i, kgamma[i]);
-			faux123_power_on_set_1[7].payload[i] = kgamma[i];
-		}
-
-		kgamma[0] = 0xd3;
-		for (i=0; i<10; i++) {
-			//pr_info("kgamma_g_n [%d] => %d \n", i, kgamma[i]);
-			faux123_power_on_set_1[8].payload[i] = kgamma[i];
-		}
+	kgamma[0] = 0xd2;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_g_p [%d] => %d \n", i, kgamma[i]);
+		faux123_power_on_set_1[7].payload[i] = kgamma[i];
 	}
+
+	kgamma[0] = 0xd3;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_g_n [%d] => %d \n", i, kgamma[i]);
+		faux123_power_on_set_1[8].payload[i] = kgamma[i];
+	}
+
 	return count;
 }
 
@@ -340,19 +325,18 @@ static ssize_t kgamma_b_store(struct device *dev, struct device_attribute *attr,
 		&kgamma[4], &kgamma[5], &kgamma[6], &kgamma[7],
 		&kgamma[8], &kgamma[9]);
 
-	if (calc_checksum(kgamma)) {
-		kgamma[0] = 0xd4;
-		for (i=0; i<10; i++) {
-			//pr_info("kgamma_b_p [%d] => %d \n", i, kgamma[i]);
-			faux123_power_on_set_1[9].payload[i] = kgamma[i];
-		}
-
-		kgamma[0] = 0xd5;
-		for (i=0; i<10; i++) {
-			//pr_info("kgamma_b_n [%d] => %d \n", i, kgamma[i]);
-			faux123_power_on_set_1[10].payload[i] = kgamma[i];
-		}
+	kgamma[0] = 0xd4;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_b_p [%d] => %d \n", i, kgamma[i]);
+		faux123_power_on_set_1[9].payload[i] = kgamma[i];
 	}
+
+	kgamma[0] = 0xd5;
+	for (i=0; i<10; i++) {
+		pr_info("kgamma_b_n [%d] => %d \n", i, kgamma[i]);
+		faux123_power_on_set_1[10].payload[i] = kgamma[i];
+	}
+
 	return count;
 }
 
