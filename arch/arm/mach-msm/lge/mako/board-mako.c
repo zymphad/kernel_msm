602afd742993044fece1df7baa446e685a7fbb9a
Michael <djmizry@gmail.com>
2013-12-24 01:06:14 -0800

    set intellidemand to proper freq's

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -2092,7 +2092,7 @@ static void __init apq8064_mako_init(void)
 {
 
 #ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
-	id_set_two_phase_freq(1134000);
+	id_set_two_phase_freq(1026000);
 #endif
 
 	if (meminfo_init(SYS_MEMORY, SZ_256M) < 0)
